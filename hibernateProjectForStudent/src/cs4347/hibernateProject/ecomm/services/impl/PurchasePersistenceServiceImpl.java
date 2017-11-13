610f6fe0c9bbf2cb36493543d8586ee18ba14c93
log-m <loganmorris15@yahoo.com>
2017-11-13 12:02:03 -0600

    Update PurchasePersistenceServiceImpl.java

--- hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/PurchasePersistenceServiceImpl.java
+++ hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/PurchasePersistenceServiceImpl.java
@@ -20,42 +20,133 @@ public class PurchasePersistenceServiceImpl implements PurchasePersistenceServic
 		this.em = em;
 	}
 	
+	/**
+	 * Create purchase given purchase
+	 * @param purchase to create
+	 */
 	@Override
 	public void create(Purchase purchase) throws SQLException, DAOException
 	{
+		try{
+			em.getTransaction().begin();
+			em.persist(purchase);
+			em.getTransaction().commit();
+		}
+		catch(Exception ex){
+			em.getTransaction().rollback();
+			throw ex; 
+		}
 	}
 
+	/**
+	 * Retrieve purchase given its id
+	 * @param id of purchase to retrieve
+	 * @return purchase id
+	 */
 	@Override
 	public Purchase retrieve(Long id) throws SQLException, DAOException
 	{
-		return null;
+		try{
+			em.getTransaction().begin();
+			Purchase purch = em.find(Purchase.class, id);
+			em.getTransaction().commit();
+			return purch;
+		}
+		catch(Exception ex){
+			em.getTransaction().rollback();
+			throw ex; 
+		}
 	}
 
+	/**
+	 * Delete purchase given its id.
+	 * @param purchase to delete
+	 */
 	@Override
 	public void update(Purchase purchase) throws SQLException, DAOException
 	{
+		try{
+			em.getTransaction().begin();
+			Purchase purch = em.find(Purchase.class, purchase.getId());
+			purch.setPurchaseDate(purchase.getPurchaseDate());
+			purch.setPurchaseAmount(purchase.getPurchaseAmount());
+			purch.setCustomer(purchase.getCustomer());
+			purch.setProduct(purchase.getProduct());
+			em.getTransaction().commit();
+		}
+		catch(Exception ex){
+			em.getTransaction().rollback();
+			throw ex; 
+		}
 	}
 
+	/**
+	 * Delete purchase given its id.
+	 * @param id id of purchase
+	 */
 	@Override
 	public void delete(Long id) throws SQLException, DAOException
 	{
+		try{
+			em.getTransaction().begin();
+			Purchase purch = em.find(Purchase.class, id);
+			em.remove(purch);
+			em.getTransaction().commit();
+		}
+		catch(Exception ex){
+			em.getTransaction().rollback();
+			throw ex; 
+		}
 	}
-
+	
+	/**
+	 * Retrieve purchases made by the given customer.
+	 * @param customerID id of customer to retrieve purchases
+	 * @return list of purchases made by customer
+	 */
 	@Override
 	public List<Purchase> retrieveForCustomerID(Long customerID) throws SQLException, DAOException
 	{
-		return null;
+		em.getTransaction().begin();
+		List<Purchase> purchs = (List<Purchase>) em.createQuery("from Purchase as p where p.customer.id =: customerID")
+			.setParameter("customerID", customerID)
+			.getResultList();
+		em.getTransaction().commit();
+		return purchs;
 	}
 
+	/**
+	 * Produce a purchase summary report for the given customer.
+	 * @param customerID id of the customer to retrieve purchase summary
+	 * @return pruchase summary for given customer
+	 */
 	@Override
 	public PurchaseSummary retrievePurchaseSummary(Long customerID) throws SQLException, DAOException
 	{
-		return null;
+		PurchaseSummary purchSum = new PurchaseSummary();
+		List<Double> results = em.createQuery("SELECT MIN(p.purchaseAmount), MAX(p.purchaseAmount), AVG(p.purchaseAmount)
+		FROM Purchase as p WHERE p.customer.id =:customerID")
+			.setParameter("customerID", customerID)
+			.getResultList();
+		purchSum.minPurchase = results.get(0);
+		purchSum.maxPurchase = results.get(1);
+		purchSum.avgPurchase = results.get(2);
+		return purchSum;
 	}
 
+	/**
+	 * Retrieve purchases made for the given product.
+	 * @param productID id of product to retrieve purchases
+	 * @return list of purchases for given product
+	 */
 	@Override
 	public List<Purchase> retrieveForProductID(Long productID) throws SQLException, DAOException
 	{
-		return null;
+		em.getTransaction().begin();
+		List<Purchase> purchs = (List<Purchase>) em.createQuery("from Purchase as p where p.product.id =: productID")
+			.setParameter("productID, productID")
+			.getResultList();
+		em.getTransaction().commit();
+		return purchs;
 	}
 }
